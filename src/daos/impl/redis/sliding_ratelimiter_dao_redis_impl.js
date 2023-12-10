c1ffe637dcfcb99516868f769f5cbd848261cd36
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-12-10 19:17:00 +0200

    Implement sliding-window-based rate limtier

--- src/daos/impl/redis/sliding_ratelimiter_dao_redis_impl.js
+++ src/daos/impl/redis/sliding_ratelimiter_dao_redis_impl.js
@@ -8,28 +8,51 @@ const timeUtils = require('../../../utils/time_utils');
 
 // Challenge 7
 const hitSlidingWindow = async (name, opts) => {
-  const client = redis.getClient();
+	const client = redis.getClient();
 
-  // START Challenge #7
-  return -2;
-  // END Challenge #7
+	const currentTimestamp = timeUtils.getCurrentTimestampMillis();
+  const windowSize = opts.interval;
+	const limiterKey = keyGenerator.getKey(
+		`limiter:${windowSize}:${name}:${opts.maxHits}`
+	);
+
+	const transaction = client.multi();
+
+	transaction.zadd(limiterKey, currentTimestamp, `${currentTimestamp}-${Math.random()}`);
+	transaction.zremrangebyscore(limiterKey, '-inf', currentTimestamp - windowSize);
+	transaction.zcard(limiterKey);
+
+	const response = await transaction.execAsync();
+	const hits = parseInt(response[2], 10);
+
+	let hitsRemaining;
+
+	if (hits > opts.maxHits) {
+		// Too many hits.
+		hitsRemaining = -1;
+	} else {
+		// Return number of hits remaining.
+		hitsRemaining = opts.maxHits - hits;
+	}
+
+	return hitsRemaining;
 };
 
 /* eslint-enable */
 
 module.exports = {
-  /**
-   * Record a hit against a unique resource that is being
-   * rate limited.  Will return 0 when the resource has hit
-   * the rate limit.
-   * @param {string} name - the unique name of the resource.
-   * @param {Object} opts - object containing interval and maxHits details:
-   *   {
-   *     interval: 1,
-   *     maxHits: 5
-   *   }
-   * @returns {Promise} - Promise that resolves to number of hits remaining,
-   *   or 0 if the rate limit has been exceeded..
-   */
-  hit: hitSlidingWindow,
+	/**
+	 * Record a hit against a unique resource that is being
+	 * rate limited.  Will return 0 when the resource has hit
+	 * the rate limit.
+	 * @param {string} name - the unique name of the resource.
+	 * @param {Object} opts - object containing interval and maxHits details:
+	 *   {
+	 *     interval: 1,
+	 *     maxHits: 5
+	 *   }
+	 * @returns {Promise} - Promise that resolves to number of hits remaining,
+	 *   or 0 if the rate limit has been exceeded..
+	 */
+	hit: hitSlidingWindow,
 };
